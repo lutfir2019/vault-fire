b09f5cef112728d8ff4d529e99fa362c0bfddca3
lutfir2019 <lutfirrahman2019@gmail.com>
2025-11-05 16:32:04 +0700

    fix(list-item): remove unusable variable

--- src/components/public-view/list-item.tsx
+++ src/components/public-view/list-item.tsx
@@ -1,11 +1,5 @@
 import { useMemo, useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import CardItem from "./card-item";
 import { useGetVaultPublicItems } from "@/hooks/usePublicView";
 
